381cf7243465ad60e2019a20449fb7e3baced7c3
Andrea <andrea.alessandro@gmail.com>
2022-01-30 19:35:32 +0100

    Added unit test for byte array builder

--- Tests/BitWiserTests/DSLTests.swift
+++ Tests/BitWiserTests/DSLTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import BitWiser
 
 class DSLTests: XCTestCase {
-    
+        
     @ByteArrayBuilder func buildArrayOfBytesFromVaridicBytes() -> [Byte] {
         Byte(0x00)
         Byte(0x01)
@@ -28,52 +28,86 @@ class DSLTests: XCTestCase {
     
     @ByteArrayBuilder func buildArrayOfBytesFromVaridicMixedByteArrayOfBytes() -> [Byte] {
         [Byte(0x00)]
-        [Byte(0x01)]
-        Byte(0x02)
-        0x03
-        [UInt8(0x04)]
-        [UInt8](repeating: 0x05, count: 1)
+        Byte(0x01)
+        0x02
+        [UInt8(0x03)]
+        [UInt8](repeating: 0x04, count: 1)
     }
     
     var clause = true
     
     @ByteArrayBuilder func buildArrayOfBytesFromVaridicMixedByteArrayOfBytesIfClause() -> [Byte] {
         [Byte(0x00)]
-        [Byte(0x01)]
-        Byte(0x02)
-        0x03
-        [UInt8(0x04)]
+        Byte(0x01)
+        0x02
+        [UInt8(0x03)]
         if clause {
-            [UInt8](repeating: 0x05, count: 1)
+            [UInt8](repeating: 0x04, count: 1)
         }
     }
     
     @ByteArrayBuilder func buildArrayOfBytesFromVaridicMixedByteArrayOfBytesIfElseClause() -> [Byte] {
         [Byte(0x00)]
-        [Byte(0x01)]
-        Byte(0x02)
-        0x03
-        [UInt8(0x04)]
+        Byte(0x01)
+        0x02
+        [UInt8(0x03)]
         if clause {
-            [UInt8](repeating: 0x05, count: 1)
+            [UInt8](repeating: 0x04, count: 1)
         } else {
+            0x04
             0x05
-            0x06
         }
     }
     
     @ByteArrayBuilder func buildArrayOfBytesFromVaridicMixedByteArrayOfBytesLoop() -> [Byte] {
         [Byte(0x00)]
-        [Byte(0x01)]
-        Byte(0x02)
-        0x03
-        [UInt8(0x04)]
-        for value in [0x05, 0x06] {
+        Byte(0x01)
+        0x02
+        [UInt8(0x03)]
+        for value in [0x04, 0x05] {
             Byte(value)
         }
     }
     
-    func testByteArrayCreationFromVaridicArrayOfBytes() throws {
-       
+    func testArrayOfBytesFromVaridicBytes() throws {
+        let value = buildArrayOfBytesFromVaridicBytes()
+        value.enumerated().forEach { (index, value) in
+            XCTAssertTrue(index == value)
+        }
+    }
+    
+    func testArrayOfBytesFromVaridicByteArrayOfBytes() throws {
+        let value = buildArrayOfBytesFromVaridicByteArrayOfBytes()
+        value.enumerated().forEach { (index, value) in
+            XCTAssertTrue(index == value)
+        }
+    }
+    
+    func testArrayOfBytesFromVaridicMixedByteArrayOfBytes() throws {
+        let value = buildArrayOfBytesFromVaridicMixedByteArrayOfBytes()
+        value.enumerated().forEach { (index, value) in
+            XCTAssertTrue(index == value)
+        }
+    }
+    
+    func testArrayOfBytesFromVaridicMixedByteArrayOfBytesIfClause() throws {
+        let value = buildArrayOfBytesFromVaridicMixedByteArrayOfBytesIfClause()
+        value.enumerated().forEach { (index, value) in
+            XCTAssertTrue(index == value)
+        }
+    }
+    
+    func testArrayOfBytesFromVaridicMixedByteArrayOfBytesIfElseClause() throws {
+        let value = buildArrayOfBytesFromVaridicMixedByteArrayOfBytesIfElseClause()
+        value.enumerated().forEach { (index, value) in
+            XCTAssertTrue(index == value)
+        }
+    }
+    
+    func testArrayOfBytesFromVaridicMixedByteArrayOfBytesLoop() throws {
+        let value = buildArrayOfBytesFromVaridicMixedByteArrayOfBytesLoop()
+        value.enumerated().forEach { (index, value) in
+            XCTAssertTrue(index == value)
+        }
     }
 }
